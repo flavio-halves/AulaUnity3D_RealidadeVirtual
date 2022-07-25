6bac776864552fe84dd0306215b34809c8e34c1e
fhalves <fhalves@live.com>
2022-07-24 21:29:54 -0300

    aula22_07_2022
    continuação da aula

---
+++ New Unity Project 1/Assets/Scripts/FunctionsButtons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunctionsButtons : MonoBehaviour {
+
+
+    public void evento(string texto)
+    {
+        Debug.Log(texto);
+    }
+
+    public void apagarLuz(GameObject luz)
+    {
+        luz.SetActive(false);
+        //luz.SetActive(enabled = true);
+    }
+}
